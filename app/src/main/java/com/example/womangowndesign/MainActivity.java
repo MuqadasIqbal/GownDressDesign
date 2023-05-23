acad64c8f30ca8bb622eabd002e0f693ce9195e1
Muqadas Iqbal <muqadasiqbal807@gmail.com>
2023-05-23 23:59:57 +0500

    StartActivity

--- app/src/main/java/com/example/womangowndesign/MainActivity.java
+++ app/src/main/java/com/example/womangowndesign/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.womangowndesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.womangowndesign.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,StartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
